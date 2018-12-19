e5007318100dc83102bd916ea8f99e36174098a5
Adel Kamel <adelkamel1982@gmail.com>
2018-12-19 22:58:51 +0100

    Edit controllers

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,5 @@
+const Product = require('../models/Product')
+
 const getIndex = (req,res) => {
 	res.render('admin', {
 		title: 'Administration des produits',
@@ -5,6 +7,15 @@ const getIndex = (req,res) => {
 	});
 }
 
+const postIndex = (req, res) => {
+	const { name, description, image, price } = req.body
+	const newProduct = new Product(name, description, image, price );
+	newProduct.save(() => {
+		res.redirect('/');
+	});
+}
+
 module.exports = {
-	getIndex: getIndex
+	getIndex: getIndex,
+	postIndex: postIndex
 }
